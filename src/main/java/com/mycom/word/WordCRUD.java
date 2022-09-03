d19e16c0f3d2144e64e66b2c8394d70aa59692ab
Ryun <rlarkfbs@gmail.com>
2022-09-03 15:32:34 +0900

    create project

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -13,9 +13,11 @@ public class WordCRUD implements ICRUD{
     }
     @Override
     public Object add() {
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();
 
+        System.out.print("뜻 입력 : ");
         String meaning = s.nextLine();
         return new Word(0, level, word, meaning);
     }
@@ -23,7 +25,7 @@ public class WordCRUD implements ICRUD{
     public void addWord(){
         Word one = (Word)add();
         list.add(one);
-        System.out.println("단어가 추가되었습니다.");
+        System.out.println("새로운 단어가 추가되었습니다.");
     }
 
     @Override
--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -10,18 +10,30 @@ public class WordManager {
         wordCRUD = new WordCRUD(s);
     }
     public int selectMenu() {
-        System.out.print("***영단어 마스터***\n\n"
+        System.out.print(
+                        "\n****************\n"
+                        +"1. 모든 단어 보기\n"
+                        +"2. 수준별 단어 보기\n"
+                        +"3. 단어 검색\n"
+                        +"4. 단어 추가\n"
+                        +"5. 단어 수정\n"
+                        +"6. 단어 삭제\n"
+                        +"7. 파일 저장\n"
+                        +"0. 나가기\n"
                         +"****************\n"
-                        +"1.모든 단어 보기\n"
-                        +"2.");
+                        +"=> 원하는 메뉴는? ");
 
         return s.nextInt();
     }
     public void start(){
+        System.out.println("\n\n***영단어 마스터***\n");
         while(true) {
             int menu = selectMenu();
-            if(menu == 0) break;
-            if(menu == 4){
+            if(menu == 0) {
+                System.out.println("단어장을 종료합니다!");
+                break;
+            }
+            else if(menu == 4){
                 wordCRUD.addWord();
                 //create
             }
